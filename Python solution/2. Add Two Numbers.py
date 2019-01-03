fde8228fda57628c3ed1a8f4f35ecc7b495c6924
Rufeer <rufeer@outlook.com>
2019-01-03 12:33:02 +0800

    Update solution of problem 2

--- Python solution/2. Add Two Numbers.py
+++ Python solution/2. Add Two Numbers.py
@@ -37,6 +37,33 @@ class Solution1:
                 node.next = l2.next
         return node
 
+class Solution2:
+    '''
+    :author: tusizi
+    :solution: O(n)
+    :runtime: 
+    '''
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        carry = 0
+        root = n = ListNode(0)
+        while l1 or l2 or carry:
+            v1 = v2 = 0
+            if l1:
+                v1 = l1.val
+                l1 = l1.next
+            if l2:
+                v2 = l2.val
+                l2 = l2.next
+            carry, val = divmod(v1+v2+carry, 10)
+            n.next = ListNode(val)
+            n = n.next
+        return root.next
+
 # Test
 # s = Solution()
 # a = ListNode(1)
