1cb75fbe6ea32b77a606db970ad7ab4e22472119
Rufeer <rufeer@outlook.com>
2019-01-03 12:07:49 +0800

    Problem 2 solved

--- 2. Add Two Numbers.py
+++ 2. Add Two Numbers.py
@@ -4,26 +4,55 @@ class ListNode:
         self.val = x
         self.next = None
 
-class Solution:
+class Solution1:
+    '''
+    :author: Rufeer
+    :solution: O(n)
+    :runtime: 188 ms
+    '''
     def addTwoNumbers(self, l1, l2):
         """
         :type l1: ListNode
         :type l2: ListNode
         :rtype: ListNode
         """
-        if l1.val == 0 and l1.next == None and l2.val == 0 and l2.next == None:
-            return ListNode(0)
+        node = ListNode((l1.val+l2.val) % 10)
+        carry = (l1.val+l2.val) // 10
+        if carry == 1:
+            if l1.next == None and l2.next == None:
+                node.next = ListNode(1)
+            elif l1.next != None and l2.next != None:
+                l1.next.val = l1.next.val + 1
+                node.next = self.addTwoNumbers(l1.next, l2.next)
+            elif l1.next != None:
+                node.next = self.addTwoNumbers(ListNode(1), l1.next)
+            elif l2.next != None:
+                node.next = self.addTwoNumbers(ListNode(1), l2.next)   
+        else:
+            if l1.next != None and l2.next != None:
+                node.next = self.addTwoNumbers(l1.next, l2.next)
+            elif l1.next != None:
+                node.next = l1.next
+            elif l2.next != None:
+                node.next = l2.next
+        return node
 
-        lsum = ListNode(0)
-        i = l1
-        j = l2
-        k = lsum
-        while i and j:
-            k.val = (i.val + j.val) % 10
-            carry = (i.val + j.val) / 10
-            k.next = ListNode(carry)
-            i = i.next
-            j = j.next
-            k = k.next
-        if i != None:
-            pass
+# Test
+# s = Solution()
+# a = ListNode(1)
+# b = ListNode(9)
+# b.next = ListNode(9)
+# a = ListNode(0)
+# b = ListNode(0)
+# a = ListNode(9)
+# b = ListNode(1)
+# b.next = ListNode(9)
+# b.next.next = ListNode(8)
+# b.next.next.next = ListNode(9)
+# a = ListNode(1)
+# a.next = ListNode(8)
+# b = ListNode(0)
+# t = s.addTwoNumbers(a, b)
+# while t:
+#     print(t.val)
+#     t = t.next
