951c257520edaf36896cd30fa9d7e4bb0459713d
Rufeer <rufeer@outlook.com>
2022-01-08 23:34:38 +0800

    update

--- Go Solution/1954.go
+++ Go Solution/1954.go
@@ -6,3 +6,20 @@ func minimumPerimeter(neededApples int64) int64 {
 	}
 	return 8 * len
 }
+
+func minimumPerimeter(neededApples int64) int64 {
+	// 外边一圈长度 12|x|^2
+	// 10^15最大边长2|x|=125992, |x|=62996
+	// 边长为2|x|时的苹果数：2len(len+1)(2len+1)
+	var left, right int64 = 0, 62996
+	for left <= right {
+		var mid int64 = (left + right) / 2
+		var sum int64 = 2 * mid * (mid + 1) * (2*mid + 1)
+		if sum < neededApples {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+	return 8 * left
+}
